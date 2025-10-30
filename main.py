b96a154a627e987bbf5172a7418df38ea42420b8
xhayper <46065349+xhayper@users.noreply.github.com>
2025-10-30 14:06:24 +0700

    feat: make knight fight 2 goblin

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 from character import Character
 
+
 def main():
     hero = Character("Knight", 30, 5)
-    enemy = Character("Goblin", 20, 3)
+    enemy_1 = Character("Goblin 1", 20, 3)
+    enemy_2 = Character("Goblin 2", 10, 3)
 
     print(hero)
-    print(enemy)
+    print(enemy_1)
+    print(enemy_2)
     print()
 
-    while hero.is_alive() and enemy.is_alive():
-        hero.attack(enemy)
-        if enemy.is_alive():
-            enemy.attack(hero)
+    while hero.is_alive() and (enemy_1.is_alive() or enemy_2.is_alive()):
+        if enemy_1.is_alive():
+            hero.attack(enemy_1)
+        elif enemy_2.is_alive():
+            hero.attack(enemy_2)
+
+        if enemy_1.is_alive():
+            enemy_1.attack(hero)
+        if enemy_2.is_alive():
+            enemy_2.attack(hero)
+
         print(hero)
-        print(enemy)
+        print(enemy_1)
+        print(enemy_2)
         print()
 
     print("Battle over!")
     if hero.is_alive():
         print(f"{hero.name} wins!")
     else:
-        print(f"{enemy.name} wins!")
+        print(f"{enemy_1.name} and {enemy_2.name} wins!")
+
 
 if __name__ == "__main__":
     main()
